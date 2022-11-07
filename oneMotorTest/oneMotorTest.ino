6fcdde09ee639594240c0cbea6be8d205f54bf73
Mr Phil Legge <phil.legge@det.nsw.edu.au>
2022-11-08 08:22:25 +1100

    Update oneMotorTest.ino

--- oneMotorTest/oneMotorTest.ino
+++ oneMotorTest/oneMotorTest.ino
@@ -10,15 +10,15 @@ L298N motor(ENA,motorIn1,motorIn2);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);  // starts the serial monitor
  
  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(counter%50 == 0){
-    motor.setSpeed(255);
+  if(counter%50 == 0){  // modulus opperator to wait for 50 cycles of the loop (rough timing method)  
+    motor.setSpeed(255); // full speed on the motor
     motor.forward();   
   }else{
     motor.backward();    
@@ -27,7 +27,7 @@ void loop() {
   }
   counter++;
 
- printSomeInfo();
+ printSomeInfo();  // function to print the info
 }
 }
 void printSomeInfo()
@@ -35,5 +35,5 @@ void printSomeInfo()
   Serial.print("Motor is moving = ");
   Serial.print(motor.isMoving());
   Serial.print(" at speed = ");
-  Serial.println(motor.getSpeed()); // println is lowercase L not capital i or 1
+  Serial.println(motor.getSpeed()); // println (is lowercase L not capital i or 1) prints with line break
 }
